a6eaa60a3c3b0ad1a5cfa6884559f1516184cd02
Dmitry Borodin <borodindr@gmail.com>
2022-01-05 15:35:04 -0600

    Updated CreateTestWorks

--- Sources/App/Migrations/CreateTestWorks.swift
+++ Sources/App/Migrations/CreateTestWorks.swift
@@ -10,19 +10,18 @@ import TinmeyCore
 
 struct CreateTestWorks: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
-        [
-            WorkAPIModel.Create(type: .cover, title: "A", description: "B", layout: .leftBody, seeMoreLink: nil),
-            WorkAPIModel.Create(type: .cover, title: "A", description: "B", layout: .middleBody, seeMoreLink: nil),
-            WorkAPIModel.Create(type: .cover, title: "A", description: "B", layout: .rightBody, seeMoreLink: nil),
-            WorkAPIModel.Create(type: .cover, title: "A", description: "B", layout: .leftLargeBody, seeMoreLink: nil),
-            WorkAPIModel.Create(type: .cover, title: "A", description: "B", layout: .rightLargeBody, seeMoreLink: nil),
-        ]
-            .enumerated()
-            .map { $1.makeWork(sortIndex: $0) }
-            .map { $0.save(on: database) }
-            .flatten(on: database.eventLoop)
+        WorkAPIModel.Create(
+            title: "A",
+            description: "A",
+            tags: ["tag"],
+            seeMoreLink: nil,
+            bodyIndex: 0,
+            images: [.init(id: nil), .init(id: nil)]
+        )
+            .makeWork(type: .cover, sortIndex: 0)
+            .save(on: database)
     }
-    
+
     func revert(on database: Database) -> EventLoopFuture<Void> {
         database.eventLoop.future()
     }

e91194343efe342b5582fbb37dbc4dbe6953af9a
Dmitry Borodin <borodindr@gmail.com>
2022-04-12 13:47:24 -0500

    Deleted profiles controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,6 @@ import Vapor
 func routes(_ app: Application) throws {
     try app.register(collection: WorksController())
     try app.register(collection: UsersController())
-    try app.register(collection: ProfileController())
     try app.register(collection: SectionsController())
     try app.register(collection: WebsiteController())
     try app.register(collection: TagsController())
